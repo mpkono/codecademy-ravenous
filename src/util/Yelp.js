ecae30ca762ebeb3e78e58ecd5d5732aac4f5ecb
mpkono <mail4mike@gmail.com>
2017-11-27 11:52:50 -0500

    Fixed Yelp call and category

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -19,12 +19,12 @@ const Yelp = {
 
   search(term, location, sortBy) {
     return Yelp.getAccessToken().then(() => {
-      fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${accessToken}`}})
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${accessToken}`}})
     }).then(response => {
       return response.json();
     }).then(jsonResponse => {
       if (jsonResponse.businesses) {
-        return jsonResponse.business.map(business => {
+        return jsonResponse.businesses.map(business => {
           return {
             id: business.id,
             imageSrc: business.image_url,
@@ -33,7 +33,7 @@ const Yelp = {
             city: business.location.city,
             state: business.location.state,
             zipCode: business.location.zip_code,
-            category: business.url,
+            category: business.categories[0].title,
             rating: business.rating,
             reviewCount: business.review_count
           };
