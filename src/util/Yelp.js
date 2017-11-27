07ecd5bbae214139f04bed045c1598713dd9ecb6
mpkono <mail4mike@gmail.com>
2017-11-27 10:44:05 -0500

    Created Yelp connectivity

---
+++ src/util/Yelp.js
@@ -0,0 +1,47 @@
+const clientId = 'M4ikzBaFzWaP0TqjVJQYiw';
+const secret = 'V3Ql8TGsnYAh5uWXuOJXLuZbP4Jre97NpX6KxlNzEtlVVhciwBQbrE5JO4y3R1Lz';
+let accessToken = '';
+
+const Yelp = {
+  getAccessToken() {
+    if (accessToken !== '') {
+      return new Promise(resolve => {
+        resolve(accessToken);
+      });
+    }
+
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {method: 'POST'}).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      accessToken = jsonResponse.access_token;
+    });
+  },
+
+  search(term, location, sortBy) {
+    return Yelp.getAccessToken().then(() => {
+      fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${accessToken}`}})
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.business.map(business => {
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.url,
+            rating: business.rating,
+            reviewCount: business.review_count
+          };
+        });
+      }
+    });
+  }
+
+};
+
+export default Yelp;
